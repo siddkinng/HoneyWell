4672b3132db2a525f8c5c1ca28af2e7527f1557a
Siddhartha Chakraborty <Siddhartha_Chakrabo1@Dellteam.com>
2020-12-12 10:52:19 +0530

    initial commit

---
+++ Controllers/UserController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using UserHoneyWell.Models;
+
+namespace UserHoneyWell.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly TestContext _context;
+
+        public UserController(TestContext testContext)
+        {
+            _context = testContext;
+        }
+
+        // POST: api/User
+        [HttpPost]
+        public async Task<ActionResult<Users>> PostMediciceTrackingSystem(Users users)
+        {
+            //validating if email is valid 
+            bool isEmail = Regex.IsMatch(users.Email, @"\A(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?)\Z", RegexOptions.IgnoreCase);
+
+            try
+            {
+                if (!string.IsNullOrWhiteSpace(users.UserName) && !string.IsNullOrWhiteSpace(users.Email) && isEmail==true)
+                {
+                    _context.USERS.Add(users);
+                }
+                else
+                {
+                    return BadRequest();
+                }
+
+                await _context.SaveChangesAsync(); //saving changes to db 
+            }
+            catch (DbUpdateException)
+            {
+               //handle exception
+            }
+
+            return CreatedAtAction("UserCreated", new { id = users.Userid }, users);
+           // return Ok(201);
+        }
+
+    }
+}
---
+++ Models/TestContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UserHoneyWell.Models
+{
+    public class TestContext :DbContext
+    {
+        public TestContext(DbContextOptions<Users> dbContextOptions): base(dbContextOptions) //scafolding issue due to some VS upgrade 
+        {
+
+        }
+
+        public virtual DbSet<Users> USERS { get; set; }
+    }
+}
